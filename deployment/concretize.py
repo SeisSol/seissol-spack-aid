85c5b23877c1407adc7abd8f6c1b01480174caa3
ravil <ravil.dorozhinskii@tum.de>
2020-12-08 19:28:09 +0100

    Added jenkinsfile

---
+++ deployment/concretize.py
@@ -0,0 +1,61 @@
+# Example:
+#   python3 ./concretize.py -i ./templates -o ./build -c gcc@8.4.0 --mpi openmpi@3.1.5^cuda --os ubuntu:18.04
+
+#spack containerize | sed 's/\(FROM \)\(.*\)\( as builder\)/\1custom-builder\3/'
+#spack containerize | sed 's/\(FROM \)\([[:alpha:]]\+:[[:digit:]]\+\.[[:digit:]]\+\)/\1custom-builder/'
+
+import argparse
+import os, errno
+import jinja2
+
+cmd_parser = argparse.ArgumentParser()
+cmd_parser.add_argument('-c','--compiler', help='compiler suite and version')
+cmd_parser.add_argument('--mpi', help='specific mpi implementation, options and version')
+cmd_parser.add_argument('--os', help='target os and its version')
+cmd_parser.add_argument('-i', '--input_dir', help='output directory')
+cmd_parser.add_argument('-a', '--arch', help='target image host arch')
+cmd_parser.add_argument('-o', '--output_dir', help='directory which contains jinja templates')
+args = cmd_parser.parse_args()
+
+OS_TRANSLATION_TABLE = {'ubuntu:16.04' : 'spack/ubuntu-xenial',
+                        'ubuntu:18.04' : 'spack/ubuntu-bionic'}
+
+def convert_to_spack_os(os_name):   
+    if not OS_TRANSLATION_TABLE[os_name]:
+        raise ValueError(f'Currently, your requested os - i.e., {os_name} - is not supported')
+    
+    return OS_TRANSLATION_TABLE[os_name]
+
+
+def write_to_file(file_path, content):
+    file = open(file_path, 'w')
+    file.write(content + '\n')
+    file.close()
+    
+
+if not os.path.exists(args.input_dir):
+    raise RuntimeError('Cannot open directory with templates')
+
+template_loader = jinja2.FileSystemLoader(searchpath=args.input_dir)
+template_env = jinja2.Environment(loader=template_loader)
+
+print('=' * 80)
+template = template_env.get_template('Dockerfile.spack.t')
+custom_builder = template.render(spack_os = convert_to_spack_os(args.os), compiler=args.compiler)
+print(custom_builder)
+
+print('=' * 80)
+template = template_env.get_template('spack.yaml.t')
+spack_spec = template.render(compiler=args.compiler, mpi=args.mpi, arch=args.arch, os=args.os)
+print(spack_spec)
+
+
+if not os.path.exists(args.output_dir):
+    raise RuntimeError('Cannot open the output directory')
+
+
+write_to_file(file_path=os.path.join(args.output_dir, 'Dockerfile.spack'), 
+              content=custom_builder)
+
+write_to_file(file_path=os.path.join(args.output_dir, 'spack.yaml'), 
+              content=spack_spec)
---
+++ deployment/templates/spack.yaml.t
@@ -0,0 +1,47 @@
+spack:
+  definitions:
+  - compilers: [{{ compiler }}]
+  - mpis: [{{ mpi }}]
+  - targets: [target={{ arch }}]
+  - packages: [seissol-env+mpi+asagi~building_tools, seissol-utils]
+
+  specs:
+  - matrix:
+    - [\$compilers]
+    - [\$targets]
+    - [+piclibs]
+  - matrix:
+    - [\$mpis, cmake@3.16.2]
+    - [\$targets]
+    - [\$%compilers]
+  - matrix:
+    - [\$packages]
+    - [\$targets]
+    - [\$^mpis]
+    - [\$%compilers]
+  
+  container:
+    format: singularity
+
+    base:
+      image: {{ os }}
+      spack: develop
+
+    strip: false
+
+    os_packages:
+      - python3
+      - python3-pip
+      - pkg-config
+      - make
+      - git
+
+    extra_instructions:
+        final: |
+            RUN update-alternatives --install /usr/bin/python python /usr/bin/python3 20 \
+                && update-alternatives --install /usr/bin/gcc python /opt/view/bin/gcc 20 \
+                && update-alternatives --install /usr/bin/g++ python /opt/view/bin/g++ 20 \
+                && ranlib /opt/view/lib/gcc/*/*/libgcc.a \
+                && pip3 install scons numpy \
+                && mkdir local_workspace
+            WORKDIR /local_workspace
