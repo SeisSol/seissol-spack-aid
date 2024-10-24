4efb98d6156eb768bc08130c0c0451cefd5705a7
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2024-10-24 15:50:19 +0200

    update tandem

--- spack/packages/tandem/package.py
+++ spack/packages/tandem/package.py
@@ -19,6 +19,7 @@ class Tandem(CMakePackage, CudaPackage, ROCmPackage):
     license("BSD-3-Clause")
 
     version("main", branch="main", submodules=True)
+    version("develop", branch="thomas/develop", submodules=True)
 
     # we cannot use the tar.gz file because it does not contains submodules
     version(
@@ -26,6 +27,8 @@ class Tandem(CMakePackage, CudaPackage, ROCmPackage):
     )
     version("1.0", tag="v1.0", commit="eccab10cbdf5842ed9903fac7a023be5e2779f36", submodules=True)
 
+    depends_on("c", type="build")  # generated
+    depends_on("cxx", type="build")  # generated
     patch("fix_v1.0_compilation.diff", when="@1.0")
 
     maintainers("dmay23", "Thomas-Ulrich")
@@ -47,7 +50,13 @@ class Tandem(CMakePackage, CudaPackage, ROCmPackage):
 
     depends_on("mpi")
 
-    for var in ["openmpi", "mpich", "mvapich", "mvapich2", "mvapich2-gdr"]:
+    for var in ["openmpi", "mpich"]:
+        for tgt in CudaPackage.cuda_arch_values:
+            depends_on(
+                f"{var} +cuda cuda_arch={tgt}", when=f"+cuda cuda_arch={tgt} ^[virtuals=mpi] {var}"
+            )
+    # these 2 are not cuda packages
+    for var in ["mvapich", "mvapich2", "mvapich2-gdr"]:
         depends_on(f"{var} +cuda", when=f"+cuda ^[virtuals=mpi] {var}")
 
     for var in ["mpich", "mvapich2-gdr"]:
@@ -60,24 +69,54 @@ class Tandem(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("eigen@3.4.0")
 
     depends_on("zlib-api")
-    depends_on("petsc@3.14.6: +int64 +mumps +scalapack memalign=32")
-    depends_on("petsc@3.14.6: +int64 +mumps +scalapack +knl", when="target=skylake:")
+    depends_on("petsc@3.16: +int64 +mumps +scalapack")
+
+    # Dictionary for architecture alignments
+    arch_alignments = {
+        "x86_64": 8,
+        "x86_64_v2": 16,
+        "x86_64_v3": 32,
+        "x86_64_v4": 64,
+        "westmere": 16,
+        "sandybridge": 32,
+        "haswell": 32,
+        "mic_knl": 64,
+        "skylake": 64,
+        "cascadelake": 64,
+        "icelake": 64,
+        "naples": 32,
+        "rome": 32,
+        "milan": 64,
+        "bergamo": 64,
+        "thunderx2": 64,
+        "power9": 64,
+        "apple-m1": 128,
+        "apple-m2": 128,
+        "a64fx": 128,
+        "aarch64": 16,  # For completeness, include aarch64 if needed
+        "neon": 16,
+        "zen": 32,
+        "zen2": 32,
+        "zen3": 64,
+        "zen4": 64,
+    }
+    for arch, align in arch_alignments.items():
+        petsc_align = max(32, align)
+        for forbidden in ("4", "8", "16", "32", "64", "128", "none"):
+            if forbidden != f"{petsc_align}":
+                conflicts(f"petsc memalign={forbidden}", when=f"target={arch}")
+    depends_on("petsc +knl", when="target=skylake:")
 
     with when("+cuda"):
         for tgt in CudaPackage.cuda_arch_values:
-            depends_on(
-                f"petsc@main +int64 +mumps +scalapack memalign=32 +cuda cuda_arch={tgt}",
-                when=f"+cuda cuda_arch={tgt}",
-            )
+            depends_on(f"petsc +cuda cuda_arch={tgt}", when=f"+cuda cuda_arch={tgt}")
     with when("+rocm"):
         for tgt in ROCmPackage.amdgpu_targets:
-            depends_on(
-                f"petsc@main +int64 +mumps +scalapack memalign=32 +rocm amdgpu_target={tgt}",
-                when=f"+rocm amdgpu_target={tgt}",
-            )
+            depends_on(f"petsc +rocm amdgpu_target={tgt}", when=f"+rocm amdgpu_target={tgt}")
 
     depends_on("python@3", type="build", when="+python")
     depends_on("py-numpy", type="build", when="+python")
+    depends_on("py-setuptools", type="build", when="+python")
 
     # see https://github.com/TEAR-ERC/tandem/issues/45
     conflicts("%intel")
@@ -102,20 +141,52 @@ class Tandem(CMakePackage, CudaPackage, ROCmPackage):
             self.define_from_variant("MIN_QUADRATURE_ORDER", "min_quadrature_order"),
         ]
 
-        arch_dic = {}
-        arch_dic["skylake"] = "skl"
-        arch_dic["skylake_avx512"] = "skx"
-        arch_dic["haswell"] = "hsw"
-        arch_dic["sandybridge"] = "snb"
-        arch_dic["zen2"] = "rome"
-        arch_dic["zen"] = "naples"
-        target = str(self.spec.target)
+        # basic family matching
+        hostarch = "noarch"
+        if self.spec.target >= "aarch64":
+            hostarch = "neon"
+        if self.spec.target >= "x86_64":
+            # pure x86_64v1 doesn't support anything above SSE3
+            hostarch = "noarch"
+        if self.spec.target >= "x86_64_v2":
+            # AVX is only required for x86_64v3 and upwards
+            hostarch = "wsm"
+        if self.spec.target >= "x86_64_v3":
+            hostarch = "hsw"
+        if self.spec.target >= "x86_64_v4":
+            hostarch = "skx"
 
-        if target in arch_dic:
-            args.append("-DARCH=" + arch_dic[target])
-        else:
-            print(target, "not in arch list of tandem, using native")
-            args.append("-DARCH=native")
+        # specific architecture matching
+        if self.spec.target >= "westmere":
+            hostarch = "wsm"
+        if self.spec.target >= "sandybridge":
+            hostarch = "snb"
+        if self.spec.target >= "haswell":
+            hostarch = "hsw"
+        if self.spec.target >= "mic_knl":
+            hostarch = "knl"
+        if self.spec.target >= "skylake_avx512":
+            hostarch = "skx"
+        if self.spec.target >= "zen":
+            hostarch = "naples"
+        if self.spec.target >= "zen2":
+            hostarch = "rome"
+        if self.spec.target >= "zen3":
+            hostarch = "milan"
+        if self.spec.target >= "zen4":
+            hostarch = "bergamo"
+        if self.spec.target >= "thunderx2":
+            hostarch = "thunderx2t99"
+        if self.spec.target >= "power9":
+            hostarch = "power9"
+        if self.spec.target >= "m1":
+            hostarch = "apple-m1"
+        if self.spec.target >= "m2":
+            hostarch = "apple-m2"
+        if self.spec.target >= "a64fx":
+            hostarch = "a64fx"
+
+        args.append(f"-DARCH={hostarch}")
 
         return args
 
