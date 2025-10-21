f2b832c06e1ee01ee65ec867a4531ecacb681d7e
Thomas-Ulrich <thomas.ulrich@geophysik.uni-muenchen.de>
2025-10-21 13:32:54 +0200

    add py-tensorforge and seissol

---
+++ spack/packages/py-tensorforge/package.py
@@ -0,0 +1,27 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class PyTensorforge(PythonPackage):
+    """A code generator for small (batched) tensor contraction kernels on GPUs"""
+
+    homepage = "https://github.com/SeisSol/TensorForge/blob/master/README.md"
+    git = "https://github.com/SeisSol/TensorForge.git"
+
+    maintainers("davschneller", "Thomas-Ulrich")
+    license("MIT")
+
+    version("master", branch="master")
+    #depends_on("py-numpy")
+    #depends_on("py-jinja2", type=("build", "run"))
+    #depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+
+    def setup_run_environment(self, env):
+        env.prepend_path("PATH", self.spec.prefix)
+        env.prepend_path("PYTHONPATH", self.spec.prefix)
--- spack/packages/seissol/package.py
+++ spack/packages/seissol/package.py
@@ -242,10 +242,13 @@ class Seissol(CMakePackage, CudaPackage, ROCmPackage):
         depends_on("py-matplotlib")
         depends_on("py-pspamm", when="gemm_tools_list=PSpaMM")
 
-        forwarded_variants = ["cuda", "intel_gpu", "rocm"]
+        forwarded_variants = ["cuda", "rocm"]
         for v in forwarded_variants:
             depends_on("py-gemmforge", when=f"+{v}")
             depends_on("py-chainforgecodegen", when=f"+{v}")
+
+        #depends_on("tiny-tensor-compiler",when="+intel_gpu")
+        depends_on("py-tensorforge",when="+intel_gpu")
 
         depends_on("libxsmm@=1.17 +generator", when="gemm_tools_list=LIBXSMM target=x86_64:")
 
