0c75aa1550886ada32722353209f5a4a5ed6d0f0
Vikas Kurapati <7347463+vikaskurapati@users.noreply.github.com>
2025-11-25 08:55:17 +0100

    Update import statement for Cxxtest package

--- spack/packages/cxxtest/package.py
+++ spack/packages/cxxtest/package.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
-from spack import *
+from spack.package import *
 
 class Cxxtest(Package):
     """CxxTest is a unit testing framework for C++."""
